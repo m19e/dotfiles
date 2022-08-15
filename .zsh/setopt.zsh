369b7b41dff9fc2f06b70e0b03d30c9575753bd6
m19e <nexus.me7290@gmail.com>
2022-08-15 21:30:37 +0900

    Remove env for vcxsrv

--- .zsh/setopt.zsh
+++ .zsh/setopt.zsh
@@ -16,7 +16,7 @@ setopt share_history
 # -----------------------------
 
 ### XLaunch
-export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
+# export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
 export LIBGL_ALWAYS_INDIRECT=1
 
 ### エディタをvimに設定
